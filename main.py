c5eb1a47dc6891c244399cc3794a6547c5fdb6b9
rafiattaui <athallah.rafi13@gmail.com>
2025-02-12 09:55:15 +0700

    added product name

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ DRIVER_PATH = "chromedriver-win64/chromedriver.exe"
 
 # Set Chrome options
 options = Options()
-# options.add_argument("--headless=new")  # Uncomment for headless mode
 options.add_argument("--window-size=1920,1200")
 options.add_argument("--disable-gpu")  
 options.add_argument("--no-sandbox")  
@@ -18,7 +17,8 @@ service = Service(DRIVER_PATH)
 driver = webdriver.Chrome(service=service, options=options)
 
 # Product to search
-product_query = "rtx 3050"
+product_query = "rtx 3060"
+keywords = []
 
 # Navigate to Tokopedia search page
 driver.get(f"https://www.tokopedia.com/search?st=product&q={product_query}")
@@ -27,11 +27,19 @@ driver.get(f"https://www.tokopedia.com/search?st=product&q={product_query}")
 driver.implicitly_wait(5)
 
 # Find all product price elements
-prices = driver.find_elements(By.XPATH, "//div[contains(@class, '_67d6E1xDKIzw+i2D2L0tjw== ')]")  # Adjusted for dynamic class names
+names = driver.find_elements(By.XPATH, "//span[contains(@class, '_0T8-iGxMpV6NEsYEhwkqEg==')]") # Product names
+prices = driver.find_elements(By.XPATH, "//div[contains(@class, '_67d6E1xDKIzw+i2D2L0tjw== ')]")  # Product prices
 
 # Print product prices
-for i, price in enumerate(prices[:3], 1):  # Limit output to the first 3 products
-    print(f"Product {i} price: {price.text}")
+for i in range(3):
+    try:
+        print(f"Product {i+1}:")
+        print("Name:", names[i].text)
+        print("Price:", prices[i].text)
+        print()
+    except:
+        print("Product not found.\n")
+        break
 
 # Close the browser
 print("Closing the browser...")
