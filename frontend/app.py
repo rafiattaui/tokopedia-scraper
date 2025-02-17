caf5dd703f0164f89d0b3dc9490c8bed40b89ea2
rafiattaui <athallah.rafi13@gmail.com>
2025-02-17 16:32:14 +0700

    made scraper an object

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+from backend.main import Scraper
+
 
 app = Flask(__name__)
+scraper = Scraper()
 
 @app.route("/")
 def index():
@@ -8,7 +11,7 @@ def index():
 
 @app.route("/search=<query>")
 def index_query():
-    return render_template()
+    return 0
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
